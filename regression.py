0fe8acad6d64c4b4cff14293ea51b6e7dfe9081c
cnadrag <nadrag.carmen@yahoo.ro>
2019-06-24 13:27:53 +0300

    Update regression.py

--- regression.py
+++ regression.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('raw_hum_pm2.5_regression.csv')
-X = dataset.iloc[:, :-1].values #get a copy of dataset exclude last column
-y = dataset.iloc[:, 1].values #get array of dataset in column 1st
+dataset = pd.read_csv('regression.csv')
+X = dataset.iloc[:, :-1].values #get HUM column
+y = dataset.iloc[:, 1].values #get PM2.5 column
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split 
@@ -29,18 +29,18 @@ y_pred = regressor.predict(X_test)
 
 # Visualizing the Training set results
 viz_train = plt
-viz_train.scatter(X_train, y_train)
+viz_train.scatter(X_train, y_train, color='red')
 viz_train.plot(X_train, regressor.predict(X_train), color='blue')
-viz_train.title('Salary VS Experience (Training set)')
-viz_train.xlabel('Year of Experience')
-viz_train.ylabel('Salary')
+viz_train.title('HUM vs PM2.5 (Training set)')
+viz_train.xlabel('HUM value')
+viz_train.ylabel('PM2.5')
 viz_train.show()
 
 # Visualizing the Test set results
 viz_test = plt
 viz_test.scatter(X_test, y_test, color='red')
 viz_test.plot(X_train, regressor.predict(X_train), color='blue')
-viz_test.title('Salary VS Experience (Test set)')
-viz_test.xlabel('Year of Experience')
-viz_test.ylabel('Salary')
+viz_test.title('HUM vs PM2.5  (Test set)')
+viz_test.xlabel('HUM value')
+viz_test.ylabel('PM2.5')
 viz_test.show()
